d8b82873f48de9883e954aa5ab653027cc3d6463
Mosaddik Bin Hafiz <mosaddikbinhafiz@gmail.com>
2016-10-08 23:45:07 +0600

    Frist Commit

---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+    using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    class Calculator
+    {
+        public string FristNumner { get; set; }
+        public string SecondNumber { get; set; }
+
+
+        public Calculator(string fristNumber , string secondNumber)
+        {
+            FristNumner = fristNumber;
+            SecondNumber = SecondNumber;
+        }
+
+
+        public int Add()
+        {
+            return Convert.ToInt32(FristNumner+SecondNumber);
+        }
+    }
+}
